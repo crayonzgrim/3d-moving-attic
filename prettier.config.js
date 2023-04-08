633f3f920bee7c61d1b4b5a9d926755b81d9aadd
crayonzgrim <crayonzgrim@gmail.com>
2023-04-09 08:08:39 +0900

    :tada: Initialized 3d chair color change

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+const options = {
+  arrowParens: "always",
+  semi: true,
+  singleQuote: true,
+  endOfLine: "lf",
+  trailingComma: "none",
+  bracketSpacing: true,
+  tabWidth: 2,
+  printWidth: 80,
+};
+
+module.exports = options;
